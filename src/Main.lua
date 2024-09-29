401fe7c556cbbf70911af40f1fe4cdf239366d3d
random19213 <todorov_spatial_1@abv.bg>
2024-09-29 17:45:57 +0300

    210942198742104721

--- src/Main.lua
+++ src/Main.lua
@@ -4,7 +4,9 @@ print("Requiring Modules")
 local s, e = pcall(function()
     for name, content in _G[_G.CLIENT_NAME] do
         if name == "Main.lua" then continue end
-        _G[_G.CLIENT_NAME][name] = task.spawn(loadstring(content))
+        task.spawn(function()
+            _G[_G.CLIENT_NAME][name] = (loadstring(content)()
+        end)
     end
 end)
 
