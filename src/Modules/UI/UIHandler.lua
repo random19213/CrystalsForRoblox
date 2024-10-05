9bdd020509c5ec870dc1d3fd542f6bb1f9719453
random19213 <todorov_spatial_1@abv.bg>
2024-10-05 18:17:08 +0300

    120941279047210947210421

--- src/Modules/UI/UIHandler.lua
+++ src/Modules/UI/UIHandler.lua
@@ -1,7 +1,19 @@
 local UIHandler = {}
 
 function UIHandler.Init()
+    local uiFramework = _G._crystalRequire("CrystalsUI.lua")
+    local app = uiFramework.new()
+    app:RunApp {
+        Name = "Crystals4Bedwars",
+        Home = app.Container{
+            Size = Udim2.fromScale(1,1),
+            BackgroundTransparency = 0.5,
+            Position = Udim2.fromScale(0.5, 0.5),
+            AnchorPoint = Vector2.new(0.5, 0.5)
+        }
+    }
 
+    _G._crystalApp = app
 end
 
 return UIHandler
