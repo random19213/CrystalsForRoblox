5802be0404b22176044f0e85a02563ca0c1dd3cb
random19213 <todorov_spatial_1@abv.bg>
2024-10-11 21:41:21 +0300

    21094821904721074021412

--- CrystalsInitialize.lua
+++ CrystalsInitialize.lua
@@ -5,8 +5,9 @@ local ReplicatedFirst = game:GetService("ReplicatedFirst")
 local Players = game:GetService("Players")
 
 local player = Players.LocalPlayer
-_G[_G.CLIENT_NAME] = {}
-_G[_G.CLIENT_NAME.."queue"] = {} 
+-- _G[_G.CLIENT_NAME] = {}
+-- _G[_G.CLIENT_NAME.."queue"] = {} 
+_G._crmodules = {}
 
 local function fetchFileFromRawURL(path)
     local url = string.format("https://raw.githubusercontent.com/%s/%s/main/%s", "random19213", "CrystalsForRoblox", path)
@@ -78,30 +79,8 @@ local function createTextFilesFromFiles(files)
             end
             parent = folder 
         end
-
-        local textValue = Instance.new("StringValue")
-        textValue.Name = segments[#segments]
-        textValue.Value = content
-        textValue.Parent = parent
-
-        local inQueue = true
-        if string.match(textValue.Name, ".client.lua$") then
-            inQueue = false
-            task.spawn(loadstring(textValue.Value)) 
-        end
-
-        if string.match(textValue.Parent.Name, "-e$") then
-            if inQueue then
-                print("Queuing script:", textValue.Name)
-                _G._initLabel.Text = "Queuing script: "..textValue.Name
-                _G[_G.CLIENT_NAME.."queue"][textValue.Name] = textValue.Value
-            end
-        else
-            inQueue = false
-            print("Loading script into active:", textValue.Name)
-            _G._initLabel.Text = "Loading script into active: "..textValue.Name
-            _G[_G.CLIENT_NAME][textValue.Name] = textValue.Value
-        end
+        
+        table.insert(_G._crmodules, 1, {Name = segments[#segments], Source = content})
     end
 end
 
--- src/CrystalsMain.lua
+++ src/CrystalsMain.lua
@@ -1,60 +1,41 @@
 local MainModule = {}
-local LoadedFirst = {}
-local StartedInitializing = false
+local RequiredModules = {}
 
 function MainModule.Initiate()
     _G._crystalRequire = function(name)
-        if not _G[_G.CLIENT_NAME][name] then
-            local queuedScript = _G[_G.CLIENT_NAME.."queue"][name]
-            if queuedScript then
-                local scriptFunction = loadstring(queuedScript)
-                if scriptFunction then
-                    _G[_G.CLIENT_NAME][name] = scriptFunction()
-                    _G[_G.CLIENT_NAME.."queue"][name] = nil
-                else
-                    warn("No loadstring function for ", name)
-                end
-            else
-                error("Script '" .. name .. "' not found in both active and queue")
-            end
-        end
-
-        if StartedInitializing == false then
-            LoadedFirst[name] = true
-        else
-            LoadedFirst[name] = nil
-        end
-
-        return _G[_G.CLIENT_NAME][name]
+        return RequiredModules[name]
     end
 
     -- require
-    StartedInitializing = true
-    local s, e = pcall(function()
-        for name, content in pairs(_G[_G.CLIENT_NAME]) do
-            print("Loading script:", name, "Type:", type(content))
+    for i, NameSource in ipairs(_G._crmodules) do
+        local name = NameSource.Name
+        local source = NameSource.Source
+
+        task.spawn(function()
             _G._initLabel.Text = "Requiring: "..name
-            if type(content) == "string" and LoadedFirst[name] == nil then
-                _G[_G.CLIENT_NAME][name] = loadstring(content)()
-            else
-                if LoadedFirst[name] == nil then
-                    print("Error: Expected string, got", type(content), "for script", name)
-                end
-            end
-        end
-    end)
-    
-    if s then
-        print("Successfully required all modules")
-    else
-        print("There was a problem requiring a module", e)
-        return
+            local result = loadstring(source)()
+            RequiredModules[name] = result
+            _G._initLabel.Text = "Successfully Required: "..name
+        end)
     end
     
+    repeat
+        task.wait()
+    until #RequiredModules == #_G._crmodules
+
     -- init
-    for name, module in _G[_G.CLIENT_NAME] do
+    for name, module in RequiredModules do
         if type(module) == "table" and module.Init then
-            task.spawn(module.Init)
+            _G._initLabel.Text = "Initializing: "..name
+            module:Init()
+        end
+    end
+
+    -- start
+    for name, module in RequiredModules do
+        if type(module) == "table" and module.Start then
+            _G._initLabel.Text = "Starting: "..name
+            module:Start()
         end
     end
 
